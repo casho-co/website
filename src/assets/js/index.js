00d622a54fe2cea7791584444dde64e6f4786669
shailendra-rc <shailendra.m@royalecheese.com>
2023-06-26 15:53:04 +0530

    website design completed with responsive

---
+++ src/assets/js/index.js
@@ -0,0 +1,8 @@
+const mobile_nav = document.querySelector(".mobile-navigation-btn");
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () => {
+  nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
