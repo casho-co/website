3b023ec8551c78d5152a07e53dff7435ca061762
shailendra-rc <shailendra.m@royalecheese.com>
2023-07-04 10:20:35 +0530

    image hover effect added in first section

--- js/index.js
+++ js/index.js
@@ -10,3 +10,25 @@ mobile_nav.addEventListener("click", () => toggleNavbar());
 jQuery(".navigation-link").on("mouseenter mouseleave", function () {
   jQuery(this).parent().siblings(".current").toggleClass("active");
 });
+
+var container = document.getElementById("container");
+var image = document.getElementById("image");
+
+container.addEventListener("mousemove", moveImage);
+
+function moveImage(event) {
+  var containerWidth = container.offsetWidth;
+  var containerHeight = container.offsetHeight;
+
+  var mouseX = event.clientX - container.offsetLeft;
+  var mouseY = event.clientY - container.offsetTop;
+
+  var imageX = (mouseX / containerWidth) * 20 - 10;
+  var imageY = (mouseY / containerHeight) * 20 - 10;
+
+  image.style.transform = "translate(" + imageX + "px, " + imageY + "px)";
+}
+
+window.addEventListener("resize", function () {
+  image.style.transform = "translate(0, 0)";
+});
