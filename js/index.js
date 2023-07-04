58ff43bc125d35f613f1adbf9288b37a2447cb96
shailendra-rc <shailendra.m@royalecheese.com>
2023-07-04 14:47:58 +0530

    typewriter effect added in token section

--- js/index.js
+++ js/index.js
@@ -32,3 +32,38 @@ jQuery(".navigation-link").on("mouseenter mouseleave", function () {
 // window.addEventListener("resize", function () {
 //   image.style.transform = "translate(0, 0)";
 // });
+
+const textLines = [
+  { text: "The Casho protocol", color: "#000000" },
+  { text: "utilises the Casho (", color: "#000000" },
+  { text: "$casho", color: "#6a38fd" },
+  { text: ") Utility Token", color: "#000000" },
+];
+
+const typingSpeed = 100;
+const textContainer = document.getElementById("text-container");
+function typeText(index) {
+  if (index >= textLines.length) {
+    return;
+  }
+
+  const line = textLines[index];
+  const { text, color } = line;
+  const lineElement = document.createElement("span");
+  lineElement.style.color = color;
+  textContainer.appendChild(lineElement);
+  var br = document.createElement("br");
+  if (index == 0) textContainer.appendChild(br);
+  let charIndex = 0;
+  const typeInterval = setInterval(() => {
+    if (charIndex < text.length) {
+      lineElement.textContent += text.charAt(charIndex);
+      charIndex++;
+    } else {
+      clearInterval(typeInterval);
+      typeText(index + 1);
+    }
+  }, typingSpeed);
+}
+
+typeText(0);
