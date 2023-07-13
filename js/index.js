bed5e4a1eb88829919673a9d841339242c354c54
shailendra-rc <shailendra.m@royalecheese.com>
2023-07-13 14:52:01 +0530

    menu issue fixed

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
-const mobile_nav = document.querySelector(".mobile-navigation-btn");
+const mobile_nav = document.querySelector(".mobile-navigation-btn img");
 const nav_header = document.querySelector(".header");
+const dropdown = document.querySelector(".dropdown-toggle");
+const dropdown_img = document.querySelector(".dropdown-toggle img");
 
-const toggleNavbar = () => {
-  nav_header.classList.toggle("active");
-};
-
-mobile_nav.addEventListener("click", () => toggleNavbar());
+document.addEventListener("click", (event) => {
+  if (event.target === mobile_nav) nav_header.classList.toggle("active");
+  else if (event.target === dropdown || event.target === dropdown_img)
+    return false;
+  else nav_header.classList.remove("active");
+  if (nav_header.classList.contains("active"))
+    document.querySelector("html").style.overflow = "hidden";
+  else document.querySelector("html").style.overflow = "unset";
+});
 
 var maxHeight = 0;
 jQuery(".token-section")
