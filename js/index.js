43cb8f550640d466bde7865bd533d88bd7905c58
shailendra-rc <shailendra.m@royalecheese.com>
2023-07-13 14:30:21 +0530

    typewriter effect code commented

--- js/index.js
+++ js/index.js
@@ -41,60 +41,60 @@ jQuery(".navigation-link").on("mouseenter mouseleave", function () {
 //   image.style.transform = "translate(0, 0)";
 // });
 
-const textLines = [
-  { text: "The Casho protocol", color: "#000000" },
-  { text: "utilises the Casho (", color: "#000000" },
-  { text: "$casho", color: "#6a38fd" },
-  { text: ") Utility Token", color: "#000000" },
-];
+// const textLines = [
+//   { text: "The Casho protocol", color: "#000000" },
+//   { text: "utilises the Casho (", color: "#000000" },
+//   { text: "$casho", color: "#6a38fd" },
+//   { text: ") Utility Token", color: "#000000" },
+// ];
 
-const typingSpeed = 100;
-const textContainer = document.getElementById("text-container");
-let typewriterStarted = false;
+// const typingSpeed = 100;
+// const textContainer = document.getElementById("text-container");
+// let typewriterStarted = false;
 
-function startTypewriter() {
-  if (typewriterStarted) {
-    return;
-  }
+// function startTypewriter() {
+//   if (typewriterStarted) {
+//     return;
+//   }
 
-  typewriterStarted = true;
+//   typewriterStarted = true;
 
-  function typeText(index) {
-    if (index >= textLines.length) {
-      return;
-    }
+//   function typeText(index) {
+//     if (index >= textLines.length) {
+//       return;
+//     }
 
-    const line = textLines[index];
-    const { text, color } = line;
-    const lineElement = document.createElement("span");
-    lineElement.style.color = color;
-    textContainer.appendChild(lineElement);
-    var br = document.createElement("br");
-    if (index == 0) textContainer.appendChild(br);
-    let charIndex = 0;
-    const typeInterval = setInterval(() => {
-      if (charIndex < text.length) {
-        lineElement.textContent += text.charAt(charIndex);
-        charIndex++;
-      } else {
-        clearInterval(typeInterval);
-        typeText(index + 1);
-      }
-    }, typingSpeed);
-  }
+//     const line = textLines[index];
+//     const { text, color } = line;
+//     const lineElement = document.createElement("span");
+//     lineElement.style.color = color;
+//     textContainer.appendChild(lineElement);
+//     var br = document.createElement("br");
+//     if (index == 0) textContainer.appendChild(br);
+//     let charIndex = 0;
+//     const typeInterval = setInterval(() => {
+//       if (charIndex < text.length) {
+//         lineElement.textContent += text.charAt(charIndex);
+//         charIndex++;
+//       } else {
+//         clearInterval(typeInterval);
+//         typeText(index + 1);
+//       }
+//     }, typingSpeed);
+//   }
 
-  typeText(0);
-}
+//   typeText(0);
+// }
 
-function checkSectionVisibility() {
-  const sectionRect = textContainer.getBoundingClientRect();
-  const isSectionVisible =
-    sectionRect.top < window.innerHeight && sectionRect.bottom >= 0;
+// function checkSectionVisibility() {
+//   const sectionRect = textContainer.getBoundingClientRect();
+//   const isSectionVisible =
+//     sectionRect.top < window.innerHeight && sectionRect.bottom >= 0;
 
-  if (isSectionVisible) {
-    startTypewriter();
-    window.removeEventListener("scroll", checkSectionVisibility);
-  }
-}
+//   if (isSectionVisible) {
+//     startTypewriter();
+//     window.removeEventListener("scroll", checkSectionVisibility);
+//   }
+// }
 
-window.addEventListener("scroll", checkSectionVisibility);
+// window.addEventListener("scroll", checkSectionVisibility);
